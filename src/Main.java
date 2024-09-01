f77ac6d41a9723fc58ffdede22be4aea9ea7399b
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-09-01 16:03:18 +0200

    First commit .

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] ages = {100, 30, 2, 1, 78, 98};
+        for (int age : ages) {
+            System.out.println(" age = " + age);
+        }
+    }
+}
